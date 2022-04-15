5b378334b474bc95e7ce4b09cb9f1c8984f4f820
robinpeng <robinpeng@tencent.com>
2022-04-15 10:49:34 +0800

    add multi url support v1

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-#./hey -n 1 -c 1 -t 30 -m GET http://11.181.252.50
 #./hey -n 100 -c 10 -t 30 -m GET -urlfile ./urlfile.txt 
-#./hey -n 100 -c 10 -t 30 -m GET -url http://11.181.252.50
+#./hey -n 100 -c 10 -t 30 -m GET -url http://x.x.x.x
