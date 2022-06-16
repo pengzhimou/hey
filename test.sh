4a8a3af26fb134a3c332e053db1b808d922d10c7
robinpeng <robinpeng@tencent.com>
2022-06-16 20:28:57 +0800

    v2

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #./hey -n 100 -c 10 -t 30 -m GET -urlfile ./urlfile.txt 每一个url都为独立的测试对象，总测试并发数=conc*url数量
 
 #./hey -n 100 -c 10 -t 30 -m GET -url http://x.x.x.x
+#./hey -n 10000 -q 100 -t 30 -m GET -url http://x.x.x.x
 
 
 #./hey -n 100 -c 50 -t 30 -m POST  -H 'X-TKE-ClusterName: cls-ogsfrmms' -H 'Content-Type: application/json' -cert ./xxx.pem -key .xxx-key.pem -url https://xxxxxxx/configmaps -D xxx.json -randmark "HEY"
